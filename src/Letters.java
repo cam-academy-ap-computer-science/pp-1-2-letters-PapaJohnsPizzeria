6f6f7b47f12f5c57cb1ac3176bb76e0c085a2b54
almajjoh000 <almajjoh000@CAM-LAB-08.bgsd.k12.wa.us>
2019-09-16 07:54:10 -0700

    complete

--- src/Letters.java
+++ src/Letters.java
@@ -4,6 +4,51 @@ public class Letters {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		diana();
+		ethan();
+		artyom();
 	}
-
+	public static void diana( ) {
+		System.out.println("Dear, Diana");
+		System.out.println("	How's life recently? I've heard of all those trips");
+		System.out.println("you've been on and i'm totally not jealous that you're");
+		System.out.println("not bringing your BFF! How are the fish? Have they eaten");
+		System.out.println("each other yet? Whatever you've been up to, I hope that I");
+		System.out.println("get an invite!");
+		common2();
+		common1();
+	}
+	public static void ethan() {
+		System.out.println("Dear, Ethan");
+		System.out.println("	You need to drop CS for an extra hour of sleep. How's your");
+		System.out.println("girlfriend? I'm sure thats going well! I heard that you're");
+		System.out.println("kind of sad because of your sleep deprivation! But thats");
+		System.out.println("nothing new. Either way, I hope that your last years on earth ");
+		System.out.println("are enjoyable.");
+		common2();
+		common1();
+	}
+	public static void artyom() {
+		System.out.println("Dear, Artyom");
+		System.out.println("	I hope that im not writing to a broke, sad, and lonely man.");
+		System.out.println("You deserve everything coming your way! You're one of the ");
+		System.out.println("nicest guys I knew growing up, and I hope that you still ");
+		System.out.println("remember me when I show up at your house tomorrow. Please don't");
+		System.out.println("do anything to me. Please.");
+		common2();
+		common1();
+	}
+	public static void common1() {
+		System.out.println("Sincerely, John");
+		System.out.println("");
+	}
+	public static void common2() {
+		System.out.println(		"Personally, i've been having a lot of fun with my classes,");
+		System.out.println("CS, especially. I've been able to express my creativity ");
+		System.out.println("with this class and I'm ready to learn a lot more.");
+		System.out.println("Figuring out how to hack into the government and be put ");
+		System.out.println("on a blacklist will be halarious. I can't wait to run for");
+		System.out.println("the rest of my life with the possibilty of never being");
+		System.out.println("able to see you again!"); //THIS IS A JOKE PLS DON'T REPORT ME!
+	}
 }
